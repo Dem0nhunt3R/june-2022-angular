e2653653c223285f3b55aaa4acc150c7f3a29da2
Dem0nhunt3R <d3m0nhunt3r92@gmail.com>
2022-10-30 14:12:27 +0200

    lesson 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
-//1
+
 import {AppComponent} from './app.component';
 import {
   CommentComponent,
